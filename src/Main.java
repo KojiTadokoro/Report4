8a7b7d3b58a4cc3d56d8dcf733e0145a6c5175ef
MatsuMatsu0129 <e175708@ie.u-ryukyu.ac.jp>
2017-11-28 01:48:00 +0900

    ステップ3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args){
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            Double value = Double.parseDouble(str);
         }catch(NullPointerException e){
             System.out.println("ぬるぽ");
             System.out.println(e.getMessage());
